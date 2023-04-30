5220bebc1c3f93942b665ca5d9ecc9cba21f80cb
lksndrv <120253467+lksndrv@users.noreply.github.com>
2023-04-30 16:18:43 +0300

    Add files via upload

--- burger.js
+++ burger.js
@@ -1,10 +1,3 @@
-// $(document).ready(function(){
-//     $('header__burher').click(function(event){
-//         $('.header__burger,.header__menu').toggleClass('active');
-//         $('body').toggleClass('lock');
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open")
---
+++ modal.js
@@ -0,0 +1,16 @@
+// Открыть модальное окно
+document.getElementById("open-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+// Закрыть модальное окно
+document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.remove("open")
+})
+// Закрыть модальное окно при клике вне его
+document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("my-modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
