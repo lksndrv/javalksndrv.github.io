b7f51090b0f6b75cee270dcde2d992f0e7c09690
lksndrv <120253467+lksndrv@users.noreply.github.com>
2023-05-01 16:22:46 +0300

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,25 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+	const accordions = document.querySelectorAll('.box5-one-item');
+
+	accordions.forEach(el => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const control = self.querySelector('.accordion__control');
+			const content = self.querySelector('.accordion__content');
+
+			self.classList.toggle('open');
+
+			// если открыт аккордеон
+			if (self.classList.contains('open')) {
+				control.setAttribute('aria-expanded', true);
+				content.setAttribute('aria-hidden', false);
+				content.style.maxHeight = content.scrollHeight + 'px';
+			} else {
+				control.setAttribute('aria-expanded', false);
+				content.setAttribute('aria-hidden', true);
+				content.style.maxHeight = null;
+			}
+		});
+	});
+});
